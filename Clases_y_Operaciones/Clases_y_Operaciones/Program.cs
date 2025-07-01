06a2162149ee00e1de041585398ed457fe92cfcc
JAtnueq <quentajoseadrian@gmail.com>
2025-07-01 08:31:39 -0300

    Add files via upload

---
+++ Clases_y_Operaciones/Clases_y_Operaciones/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_y_Operaciones
+{
+    class Operaciones
+    {
+        public int x, y, resultado;
+        public void datos()
+        {
+            Console.WriteLine("Ingresar datos (X, Y)");
+            Console.Write("X = ");
+            x = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Y = ");
+            y = Convert.ToInt16(Console.ReadLine());
+        }
+        public void suma()
+        {
+            resultado = x + y;
+            Console.WriteLine("x + y = " + resultado);
+        }
+        public void resta()
+        {
+            resultado = x - y;
+            Console.WriteLine("x - y = " + resultado);
+        }
+        public void multiplicacion()
+        {
+            resultado = x * y;
+            Console.WriteLine("x * y = " + resultado);
+        }
+        public void division()
+        {
+            resultado = x / y;
+            Console.WriteLine("x / y = " + resultado);
+            resultado = x % y;
+            Console.WriteLine("Con un resto de: " + resultado);
+        }
+    }
+            
+    class main
+    {
+        static void Main(string[] args)
+        {
+           Operaciones op = new Operaciones();
+           op.datos();
+           op.suma();
+           op.resta();
+           op.multiplicacion();
+           op.division();
+           Console.ReadKey();
+        }
+    }
+}
