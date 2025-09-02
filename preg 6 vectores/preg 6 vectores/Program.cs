d851402e3b9d4ab7b4ca86e4a792c32cf1cd86f8
JAtnueq <quentajoseadrian@gmail.com>
2025-09-02 11:08:16 -0300

    Add files via upload

---
+++ preg 6 vectores/preg 6 vectores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Visualizar, contar y sumar los elementos pares que ocupan las posiciones
+              impares de lista anterior. Imprimir las posiciones que ocupan dichos elementos
+              en la lista.*/
+            int capacidad = 10;
+            int[] numero = new int[capacidad];
+            for (int i = 0; i < capacidad; i++)
+            {
+                Console.WriteLine("Introduce el dato numero {0}", i + 1);
+                numero[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int suma = 0;
+            Console.WriteLine("--- Elementos en posiciones impares: ---");
+
+            for (int i = 0; i < capacidad; i++)
+            {
+                if (i % 2 != 0) // si el índice es impar
+                {
+                    if (numero[i] % 2 == 0)
+                    {
+                        Console.WriteLine("Posición {0}: {1}", i + 1, numero[i]);
+                        suma += numero[i];
+                    }
+                }
+            }
+
+            Console.WriteLine("La suma de los elementos pares en posiciones impares es: {0}", suma);
+
+            Console.ReadKey();
+        }
+    }
+}
