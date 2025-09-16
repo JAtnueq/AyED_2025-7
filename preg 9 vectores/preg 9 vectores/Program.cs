1bf55e5134ca5d8889ec1365dfdb575bae6a424a
JAtnueq <quentajoseadrian@gmail.com>
2025-09-16 08:57:56 -0300

    Add files via upload

---
+++ preg 9 vectores/preg 9 vectores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se ingresan 10 enteros. La computadora muestra, a continuación, 
+              aquellos que son pares.*/
+            int capacidad = 10;
+            int[] numero = new int[capacidad];
+            for (int i = 0; i < capacidad; i++)
+            {
+                Console.WriteLine("Introduce el dato numero {0}", i);
+                numero[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("--- Elementos pares: ---");
+
+            for (int i = 0; i < capacidad; i++)
+            {
+                if (numero[i] % 2 == 0) // si el índice es par
+                {
+                    Console.WriteLine("Posición {0}: {1}", i, numero[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
