b46ed68ea8b1a493ba93714ecca3f7019ca73265
JAtnueq <quentajoseadrian@gmail.com>
2025-10-09 10:58:47 -0300

    Add files via upload

---
+++ preg 20 matrices/preg 20 matrices/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preg_20_matrices
+{
+    class funciones
+    {
+        public void traza_matriz(ref int[,] matriz, ref int n)
+        {
+            //Birdbrain y Liar Dancer son vida :Vv
+            int suma = 0;
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    if (x == y)
+                    {
+                        suma += matriz[x, y];
+                    }
+                }
+            }
+            Console.WriteLine("Suma de la traza de la matriz: " + suma);
+        }
+        public void diagonal_secundaria(int[,] matriz, int n)
+        {
+            //Birdbrain Y Liar Dancer son vida
+            int suma = 0;
+            for (int x = n; x >= 0; x--)
+            {
+                for (int y = n; y <= 0; y--)
+                {
+                    Console.WriteLine("Introduce el elemento numero [{0},{1}]", x, y);
+                    matriz[x, y] = Convert.ToInt32(Console.ReadLine());
+                    if (x == y)
+                    {
+                        suma += matriz[x, y];
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*Se ingresa una matriz de NxN componentes. La computadora calcula y muestra:
+               a) La traza de la matriz (suma de los elementos de la diagonal principal)
+               b) La suma de los elementos de la otra diagonal*/
+            int n = 0;
+            Console.WriteLine("--- Tamaño Vector ---");
+            Console.WriteLine("¿De cuantos elementos sera el vector?");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--- Llenar el vector ---");
+            //--------------------------
+            int[,] matriz = new int[n, n];
+            //--------------------------
+            for (int x = 0; x < n; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    Console.WriteLine("Introduce el elemento numero [{0},{1}]", x, y);
+                    matriz[x, y] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            funciones f = new funciones();
+            f.traza_matriz(ref matriz, ref n);
+        }
+    }
+}
