6d6debee16afc9675badcddc2b04598c393f8a12
JAtnueq <quentajoseadrian@gmail.com>
2025-10-14 11:02:18 -0300

    Add files via upload

---
+++ preg 2 ArrayList/preg 2 ArrayList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preg_2_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList miLista = new ArrayList();
+            miLista.Add("Teto");
+            miLista.Add("Azul");
+            miLista.Add("Amarillo");
+            miLista.Add("Hola mundo");
+            miLista.Add("juas juas");
+            miLista.Add(":Vvvv");
+            Console.WriteLine("Contenido actual:");
+            foreach (string frase in miLista)
+                Console.WriteLine(frase);
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("¡Otra vez!");
+                foreach (string frase in miLista)
+                    Console.WriteLine(frase);
+            }
+            Console.ReadKey();
+        }
+    }
+}
