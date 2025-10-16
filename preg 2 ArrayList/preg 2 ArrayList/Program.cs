51ed45395ebe1b3c0588c1c960c224c8f8ee1487
JAtnueq <quentajoseadrian@gmail.com>
2025-10-16 09:24:43 -0300

    Program.cs

--- preg 2 ArrayList/preg 2 ArrayList/Program.cs
+++ preg 2 ArrayList/preg 2 ArrayList/Program.cs
@@ -1,16 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace preg_2_ArrayList
+namespace preg_3_ArrayList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*Escribe un programa Csharp para iterar por todos los elementos de una
+              lista de arrays.*/
             ArrayList miLista = new ArrayList();
             miLista.Add("Teto");
             miLista.Add("Azul");
@@ -21,13 +23,15 @@ namespace preg_2_ArrayList
             Console.WriteLine("Contenido actual:");
             foreach (string frase in miLista)
                 Console.WriteLine(frase);
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("¡Otra vez!");
+                Console.ReadLine();
                 foreach (string frase in miLista)
                     Console.WriteLine(frase);
             }
             Console.ReadKey();
+            /*Esto fue segun lo que entendi XDD*/
         }
     }
 }
