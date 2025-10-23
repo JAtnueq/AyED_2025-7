93aaf94cd87275ca84c64a2abb964406370cc3b9
JAtnueq <quentajoseadrian@gmail.com>
2025-10-23 09:00:40 -0300

    Update Program.cs

--- preg 1 ArraysList/preg 1 ArraysList/Program.cs
+++ preg 1 ArraysList/preg 1 ArraysList/Program.cs
@@ -1,28 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Collections; // Sin esta linea, no te toma las declaraciones de ArrayList
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace preg_1_ArraysList
+namespace preg_4_ArrayList
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*Escribe un programa CSharp para crear una lista de arrays, añadir
-              colores (cadenas) e imprimir la colección.*/
+            /*Escriba un programa Csharp para recuperar un elemento (en un índice
+              específico) de una lista de arrays dada.*/
             ArrayList miLista = new ArrayList();
-            miLista.Add("Teto");
-            miLista.Add("Azul");
-            miLista.Add("Amarillo");
-            miLista.Add("Hola mundo");
-            miLista.Add("juas juas");
-            miLista.Add(":Vvvv");
-            Console.WriteLine("Contenido actual:");
-            foreach (string frase in miLista)
-                Console.WriteLine(frase);
+            //------------Contenido del Array--------------
+            miLista.Add("Cobre");
+            miLista.Add("Oro");
+            miLista.Add("Diamante");
+            miLista.Add("Amatista");
+            miLista.Add("Jade");
+            miLista.Add("Rubi");
+            miLista.Add("Hierro");
+            miLista.Add("Tito Calderon");
+            //- 0-6 elementos -----------------------------
+            Console.WriteLine("Contenido actual del Array:");
+            for (int i = 0; i < miLista.Count; i++)
+            {
+                Console.Write("\nElemento [{0}] : ", i); Console.WriteLine(miLista[i]); 
+            }
+                
+            Console.WriteLine("\nPresiona enter para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+
+            int x;
+
+            Console.WriteLine("Que elemento quiere recuperar");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(miLista[x]);
+            Console.WriteLine("¡Recuperado!");
+            Console.ReadLine();
+
             Console.ReadKey();
         }
     }
