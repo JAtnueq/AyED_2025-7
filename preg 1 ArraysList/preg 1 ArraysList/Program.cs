4eb10bcc135657ec81b4f3a018c23b4fe0130c1c
JAtnueq <quentajoseadrian@gmail.com>
2025-10-23 09:31:06 -0300

    IMPORTANTE linea 3 del programa

--- preg 1 ArraysList/preg 1 ArraysList/Program.cs
+++ preg 1 ArraysList/preg 1 ArraysList/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Collections;
+using System.Collections; // Sin esta linea, el Visual Studio no sabe que es un ArrayList 
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,3 +31,4 @@ namespace preg_3_ArrayList
         }
     }
 }
+
