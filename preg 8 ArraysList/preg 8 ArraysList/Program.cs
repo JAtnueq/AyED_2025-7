d74b4cfeff29e8ab5dedd4730b5c628f5f4a81d6
JAtnueq <quentajoseadrian@gmail.com>
2025-10-28 09:08:26 -0300

    Add files via upload

---
+++ preg 8 ArraysList/preg 8 ArraysList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace preg_4_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribe un programa en Csharp para ordenar una lista de arrays.*/
+            ArrayList miLista = new ArrayList();
+            //------------Contenido del Array--------------
+            miLista.Add("Fisura");
+            miLista.Add("Quebrado");
+            miLista.Add("Polenta");
+            miLista.Add("Mufa");
+            miLista.Add("Frio");
+            miLista.Add("Burro");
+            miLista.Add("Animal");
+            miLista.Add("Gato");
+            miLista.Add("Dictadura");
+            miLista.Add("Mexico");
+            miLista.Add("Peru");
+            miLista.Add("Clave");
+            miLista.Add("Piedru");
+            miLista.Add("Cerru");
+            //- 7 elementos -----------------------------
+            // (Tambien se cuenta el "0")
+            Console.WriteLine("Contenido actual del Array:");
+            Console.WriteLine("--------------------------------------");
+            for (int i = 0; i < miLista.Count; i++)
+            {
+                Console.Write("Elemento [{0}] : ", i); Console.WriteLine(miLista[i]);
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\nPresiona enter para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            Console.WriteLine("El Array esta desordenado, presiona Enter para ordenarlo");
+            Console.ReadLine();
+            miLista.Sort();
+            //---------------------------------------
+
+            Console.WriteLine("\nLista actualizada");
+            Console.WriteLine("--------------------------------------");
+            foreach (string frase in miLista)
+                Console.WriteLine(frase);
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\n(La lista se ordeno por orden alfabetico. Ejemplo: a, b, c, etc.)");
+            Console.ReadKey();
+
+        }
+    }
+
+}
